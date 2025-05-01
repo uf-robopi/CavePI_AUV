68d6790179c8576d522350b0d3adf34c02acbe2f
Adnan Abdullah <adnanabdullah@ufl.edu>
2025-05-01 15:26:50 -0400

    nano ros setup

--- auv_nano/setup.py
+++ auv_nano/setup.py
@@ -12,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob(os.path.join('launch', '*.py'))),
         ('share/' + package_name + '/config', ['config/config.yaml']),
         ('share/' + package_name + '/weights', glob(os.path.join('weights', '*'))),
     ],
